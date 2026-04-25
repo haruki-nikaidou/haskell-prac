5e04ef4bef66329b8d7d3da2075ad6660f0a06c7
Nikaidou Haruki <nharuki@pm.me>
2026-04-25 21:08:41 +0900

    solve a

--- src/ABC455/A.hs
+++ src/ABC455/A.hs
@@ -1,8 +1,17 @@
 module ABC455.A where
 
-import Read (readInts)
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+
+readInts :: IO [Int]
+readInts = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+
+printAns :: Bool -> IO ()
+printAns True = putStrLn "Yes"
+printAns False = putStrLn "No"
 
 main' :: IO ()
 main' = do
-  n <- readInts
-  print $ sum n
+  [a, b, c] <- readInts
+  let ans = a /= b && b == c
+  printAns ans
