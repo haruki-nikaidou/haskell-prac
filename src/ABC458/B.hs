ede4513f17f42be74cdc1884cc78e0ff89d16caf
Nikaidou Haruki <nharuki@pm.me>
2026-05-16 21:13:01 +0900

    B

--- src/ABC458/B.hs
+++ src/ABC458/B.hs
@@ -6,14 +6,32 @@ import Data.Maybe (fromJust)
 readInts :: IO [Int]
 readInts = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
 
-solve :: Int -> Int -> Int
-solve a b = a + b
+solve :: Int -> Int -> [[Int]]
+solve h w
+  | h == 1 && w == 1 = [[0]]
+  | h == 1 = [1 : replicate (w - 2) 2 ++ [1]]
+  | w == 1 = [[1] ++ replicate (h - 2) 2 ++ [1]]
+  | otherwise = grid h w
 
-printAns :: Int -> IO ()
-printAns ans = putStrLn (show ans)
+grid :: Int -> Int -> [[Int]]
+grid rows cols =
+  [ [cellAt r c | c <- [0 .. cols - 1]]
+  | r <- [0 .. rows - 1]
+  ]
+  where
+    cellAt r c
+      | isCorner r c = 2
+      | isEdge r c = 3
+      | otherwise = 4
+
+    isCorner r c = (r == 0 || r == rows - 1) && (c == 0 || c == cols - 1)
+    isEdge r c = r == 0 || r == rows - 1 || c == 0 || c == cols - 1
+
+printAns :: [[Int]] -> IO ()
+printAns ans = mapM_ (putStrLn . unwords . map show) ans
 
 main :: IO ()
 main = do
-  [a, b] <- readInts
-  let ans = solve a b
+  [h, w] <- readInts
+  let ans = solve h w
   printAns ans
