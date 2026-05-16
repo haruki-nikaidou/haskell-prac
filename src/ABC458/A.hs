63894bead935a3765b85b3820969a17b7f56e899
Nikaidou Haruki <nharuki@pm.me>
2026-05-16 21:06:58 +0900

    a

--- src/ABC458/A.hs
+++ src/ABC458/A.hs
@@ -3,17 +3,27 @@ module ABC458.A where
 import qualified Data.ByteString.Char8 as BS
 import Data.Maybe (fromJust)
 
+readInt :: IO Int
+readInt = fst . fromJust . BS.readInt <$> BS.getLine
+
 readInts :: IO [Int]
 readInts = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
 
-solve :: Int -> Int -> Int
-solve a b = a + b
+readString :: IO BS.ByteString
+readString = BS.getLine
+
+slice :: Int -> Int -> BS.ByteString -> BS.ByteString
+slice a b bs = BS.take (b - a) (BS.drop a bs)
+
+solve :: BS.ByteString -> Int -> BS.ByteString
+solve s remove_count = slice remove_count (BS.length s - remove_count) s
 
 printAns :: Int -> IO ()
 printAns ans = putStrLn (show ans)
 
 main :: IO ()
 main = do
-  [a, b] <- readInts
-  let ans = solve a b
-  printAns ans
+  s <- readString
+  remove_count <- readInt
+  let ans = solve s remove_count
+  BS.putStrLn ans
