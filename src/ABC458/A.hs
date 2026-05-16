a7c6df149688a2193202bb10b72fcc7a12e91c51
Nikaidou Haruki <nharuki@pm.me>
2026-05-16 16:21:12 +0900

    prepare ABC458

---
+++ src/ABC458/A.hs
@@ -0,0 +1,19 @@
+module ABC458.A where
+
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+
+readInts :: IO [Int]
+readInts = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+
+solve :: Int -> Int -> Int
+solve a b = a + b
+
+printAns :: Int -> IO ()
+printAns ans = putStrLn (show ans)
+
+main :: IO ()
+main = do
+  [a, b] <- readInts
+  let ans = solve a b
+  printAns ans
