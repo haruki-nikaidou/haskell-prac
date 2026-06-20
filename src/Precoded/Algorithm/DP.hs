c62449aedc37fa93b392f582418b0fc409e71bfd
Nikaidou Haruki <nharuki@pm.me>
2026-06-20 20:47:50 +0900

    implement knapsack

--- src/Precoded/Algorithm/DP.hs
+++ src/Precoded/Algorithm/DP.hs
@@ -37,3 +37,35 @@ mkGraph depsOf combine = do
             modifyIORef' reg (M.insert v n)
             pure n
   pure getNode
+
+data KnapsackItem = KnapsackItem {weight :: Int, value :: Int}
+
+knapsack :: Int -> [KnapsackItem] -> IO Int
+knapsack capacity items = do
+  getNode <- mkGraph depsOf combine
+  getNode (enc n capacity) >>= force
+  where
+    n = length items
+    -- 1-indexed map of items so vertex i corresponds to itemMap ! i
+    itemMap = M.fromList (zip [1 ..] items)
+
+    -- Encode the table cell (i, w) as a single vertex.
+    enc i w = i * (capacity + 1) + w
+    rowOf v = v `div` (capacity + 1) -- i: number of items considered
+    colOf v = v `mod` (capacity + 1) -- w: remaining capacity
+
+    depsOf v
+      | i == 0 = []
+      | weight item <= w = [enc (i - 1) w, enc (i - 1) (w - weight item)]
+      | otherwise = [enc (i - 1) w]
+      where
+        i = rowOf v
+        w = colOf v
+        item = itemMap M.! i
+
+    -- vs lines up with depsOf: [skip] or [skip, take].
+    combine v vs = case (rowOf v, vs) of
+      (0, _) -> 0
+      (i, [skip]) -> skip
+      (i, [skip, take']) -> max skip (value (itemMap M.! i) + take')
+      _ -> 0
