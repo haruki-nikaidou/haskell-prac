26b13031eacf5b079aa581d4ec926aa004f58d4e
Nikaidou Haruki <nharuki@pm.me>
2026-06-20 21:16:42 +0900

    b

--- src/ABC463/B.hs
+++ src/ABC463/B.hs
@@ -1,37 +1,51 @@
 module ABC463.B where
 
+import Control.Monad (replicateM)
 import qualified Data.ByteString.Char8 as BS
+import Data.List (transpose)
 import Data.Maybe (fromJust)
 
-readInts :: IO [Int]
-readInts = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+readIntChar :: IO (Int, Char)
+readIntChar = do
+  bs <- BS.getLine
+  let [int_word, char_word] = BS.words bs
+  let int_val = fst . fromJust $ BS.readInt int_word
+  let char_val = BS.head char_word
+  return (int_val, char_val)
 
-solve :: Int -> Int -> [[Int]]
-solve h w
-  | h == 1 && w == 1 = [[0]]
-  | h == 1 = [1 : replicate (w - 2) 2 ++ [1]]
-  | w == 1 = [[1] ++ replicate (h - 2) 2 ++ [1]]
-  | otherwise = grid h w
+parseChar :: Char -> Int
+parseChar 'A' = 0
+parseChar 'B' = 1
+parseChar 'C' = 2
+parseChar 'D' = 3
+parseChar 'E' = 4
 
-grid :: Int -> Int -> [[Int]]
-grid rows cols =
-  [ [cellAt r c | c <- [0 .. cols - 1]]
-  | r <- [0 .. rows - 1]
-  ]
+readQueries :: (BS.ByteString -> q) -> Maybe Int -> IO [q]
+readQueries f (Just n) = map f <$> replicateM n BS.getLine
+readQueries f Nothing = map f . BS.lines <$> BS.getContents
+
+parseQuery :: BS.ByteString -> [Bool]
+parseQuery line = map parseChar (BS.unpack line)
   where
-    cellAt r c
-      | isCorner r c = 2
-      | isEdge r c = 3
-      | otherwise = 4
+    parseChar 'o' = True
+    parseChar 'x' = False
 
-    isCorner r c = (r == 0 || r == rows - 1) && (c == 0 || c == cols - 1)
-    isEdge r c = r == 0 || r == rows - 1 || c == 0 || c == cols - 1
+foldOrBoolMatrix :: [[Bool]] -> [Bool]
+foldOrBoolMatrix = map or . transpose
 
-printAns :: [[Int]] -> IO ()
-printAns ans = mapM_ (putStrLn . unwords . map show) ans
+solve :: Int -> [[Bool]] -> Bool
+solve query_on bool_vectors =
+  let folded = foldOrBoolMatrix bool_vectors
+   in folded !! query_on
+
+printAns :: Bool -> IO ()
+printAns True = putStrLn "Yes"
+printAns False = putStrLn "No"
 
 main :: IO ()
 main = do
-  [h, w] <- readInts
-  let ans = solve h w
+  (_, query_char) <- readIntChar
+  let query_on = parseChar query_char
+  bool_vectors <- readQueries parseQuery Nothing
+  let ans = solve query_on bool_vectors
   printAns ans
